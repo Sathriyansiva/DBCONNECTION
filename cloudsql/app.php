a0429e62cbc8fe5c1bdddbbb6e41015be0d8fd06
Sathriyansiva <sathriyan8@gmail.com>
2017-05-31 12:11:38 +0530

    Update app.php

--- cloudsql/app.php
+++ cloudsql/app.php
@@ -30,42 +30,26 @@ $app['pdo'] = function ($app) {
         $app['mysql.password']
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->query('CREATE TABLE IF NOT EXISTS visits ' .
-        '(time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, user_ip CHAR(64))');
+    
     return $pdo;
 };
 
 $app->get('/', function (Application $app, Request $request) {
     $ip = $request->GetClientIp();
-    // Keep only the first two octets of the IP address.
-    $octets = explode($separator = ':', $ip);
-    if (count($octets) < 2) {  // Must be ip4 address
-        $octets = explode($separator = '.', $ip);
-    }
-    if (count($octets) < 2) {
-        $octets = ['bad', 'ip'];  // IP address will be recorded as bad.ip.
-    }
-    // Replace empty chunks with zeros.
-    $octets = array_map(function ($x) {
-        return $x == '' ? '0' : $x;
-    }, $octets);
-    $user_ip = $octets[0] . $separator . $octets[1];
-
-    // Insert a visit into the database.
-    /** @var PDO $pdo */
+    // Keep only the first two octets of the IP address
     $pdo = $app['pdo'];
     
 
     // Look up the last 10 visits
     $select = $pdo->prepare(
-        'SELECT * FROM chat ORDER BY created DESC LIMIT 10');
+        'SELECT * FROM chat ');
     $select->execute();
-    $visits = ["Last 10 visits:"];
+   
     while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
-        array_push($visits, sprintf('Time: %s Addr: %s', $row['created'],
+        array_push(sprintf($row['created'],
             $row['message']));
     }
-    return new Response(implode("\n", $visits), 200,
+    return new Response(implode("\n"), 200,
         ['Content-Type' => 'text/plain']);
 });
 # [END example]
