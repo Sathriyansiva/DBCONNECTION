e0450b9926f66f3d969585e42cd4d372cf67fb3e
Sathriyansiva <sathriyan8@gmail.com>
2017-06-01 15:43:23 +0530

    Update app.php

--- cat_list/app.php
+++ cat_list/app.php
@@ -1,11 +1,7 @@
 <?php
-
-# [START example]
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-		
-// create the Silex application
 $app = new Application();
 $app['pdo'] = function ($app) {
     $pdo = new PDO(
@@ -19,10 +15,7 @@ $app['pdo'] = function ($app) {
 };
 $app->get('/', function (Application $app, Request $request) {
     $ip = $request->GetClientIp();
-    // Keep only the first two octets of the IP address
-    $pdo = $app['pdo'];
-    
- $octets = explode($separator = ':', $ip);
+    $octets = explode($separator = ':', $ip);
     if (count($octets) < 2) {  // Must be ip4 address
         $octets = explode($separator = '.', $ip);
     }
@@ -34,29 +27,31 @@ $app->get('/', function (Application $app, Request $request) {
         return $x == '' ? '0' : $x;
     }, $octets);
     $user_ip = $octets[0] . $separator . $octets[1];
-    // Look up the last 10 visits
-if(isset($_GET['id']))
+    // Insert a visit into the database.
+    /** @var PDO $pdo */
+    $pdo = $app['pdo'];
+   if(isset($_GET['id']))
 {
 $id=$_GET['id'];
 }
 
- $format = strtolower($_GET['format']) == 'json'; //xml is the default
-
+$format = strtolower($_GET['format']) == 'json'; //xml is the default
+    // Look up the last 10 visits
+	
+	
     $select = $pdo->prepare(
-        'SELECT DATE_FORMAT(fromdate,'%H:%i') as time,prod_name,DATE_FORMAT(fromdate, '%Y-%m-%d') as fromdate,
-	DATE_FORMAT(todate, '%Y-%m-%d') as todate,points FROM do_product_hdr where prod_cate=:id1');
-    $select->execute(array(':id1'=>$id));
-   
+        'SELECT DATE_FORMAT(fromdate,'%H:%i') as time,prod_name,DATE_FORMAT(fromdate, '%Y-%m-%d') 
+	as fromdate,DATE_FORMAT(todate, '%Y-%m-%d') as todate,points FROM do_product_hdr where prod_cate=:id1');
+    $select->execute(array(':id1'=>$ibo));
     $visits = [""];
-    $format = strtolower($_GET['format']) == 'json';
-    while ($row = $select->fetch(PDO::FETCH_ASSOC)) {      
-		$prod_name= $row['prod_name'];
-		$time= $row['time'];
-		$fromdate1= $row['fromdate'];
-		$todate1= $row['todate'];
-		$points= $row['points'];
-		$fromdate= strtotime($row['fromdate']);
-		$todate= strtotime($row['todate']);
+    while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
+    $prod_name= $rows['prod_name'];
+		$time= $rows['time'];
+		$fromdate1= $rows['fromdate'];
+		$todate1= $rows['todate'];
+		$points= $rows['points'];
+		$fromdate= strtotime($rows['fromdate']);
+		$todate= strtotime($rows['todate']);
 		
 $timeDiff = abs($todate - $fromdate);
 
@@ -64,10 +59,10 @@ $numberDays = $timeDiff/86400;  // 86400 seconds in one day
 
 // and you might want to convert to integer
 $numberDays = intval($numberDays);
- $posts[] = array('prod_name' => $prod_name,'fromdate' =>$fromdate1, 'todate' =>$todate1,'interval'=>$numberDays,'time'=>$time,'points'=>$points);
+ $posts[] = array('prod_name' => $prod_name,'fromdate' =>$fromdate1, 'todate' =>$todate1,
+		  'interval'=>$numberDays,'time'=>$time,'points'=>$points);	
     }
-	
-    if($format == 'json') {
+	if($format == 'json') {
     header('Content-type: application/json');
     echo json_encode(array('posts'=>$posts));
   }
@@ -89,11 +84,8 @@ $numberDays = intval($numberDays);
     }
     echo '';
   }
-	
-	
-
-	 return new Response(implode("\n", $visits), 200,
-        ['Content-Type' => 'json']);
+	return new Response(implode("\n", $visits), 200,
+        ['Content-Type' => 'text/plain']);
 });
 # [END example]
 return $app;
