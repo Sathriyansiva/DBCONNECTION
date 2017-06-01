c61c06e06e4560ca7e69a0755067cc4c61f5f942
Sathriyansiva <sathriyan8@gmail.com>
2017-06-01 13:47:51 +0530

    Update app.php

--- cloudsql/app.php
+++ cloudsql/app.php
@@ -1,26 +1,7 @@
 <?php
-/**
- * Copyright 2016 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-# [START example]
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-// create the Silex application
 $app = new Application();
 
 $app['pdo'] = function ($app) {
@@ -36,7 +17,6 @@ $app['pdo'] = function ($app) {
 
 $app->get('/', function (Application $app, Request $request) {
     $ip = $request->GetClientIp();
-    // Keep only the first two octets of the IP address.
     $octets = explode($separator = ':', $ip);
     if (count($octets) < 2) {  // Must be ip4 address
         $octets = explode($separator = '.', $ip);
