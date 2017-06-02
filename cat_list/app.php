f5368a526837d6561f1a4c18b4e8edce18904a81
Sathriyansiva <sathriyan8@gmail.com>
2017-06-02 12:22:41 +0530

    Update app.php

--- cat_list/app.php
+++ cat_list/app.php
@@ -161,8 +161,8 @@ $receiveribo=$_GET['receiveribo'];
 	}
 	else if($_GET['name']=='login')
 	{
-$username = "sa@gmail.com";
-$password = "123";
+$username =$_POST['username'];
+$password = $_POST['password'];
  
 $select = $pdo->prepare(
         'SELECT * FROM  distributor_profile_hdr where Email=:username1 and Password=:password1');
@@ -172,6 +172,71 @@ $select = $pdo->prepare(
      $username= $row['Email'];
  }
 if($username != '' ){
+echo 'success';
+}else{
+echo 'failiure';
+}	
+}
+	else if($_GET['name']=='forgot')
+	{
+$email=$_POST['email'];
+ 
+$select = $pdo->prepare(
+        'SELECT * FROM `distributor_profile_hdr` where Email=:email1');
+    $select->execute(array(':email1'=>$email));
+    $visits = [""];
+    while ($rows = $select->fetch(PDO::FETCH_ASSOC)) {
+     $username= $rows['Email'];
+ }
+if($username != '' ){
+ while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
+	 $username=$row['Username'];
+	$password=$row['Password'];
+	$fname=$row['Firstname'];
+ }
+	$to = $email;
+	$subject = $fname.", your Enrollment is CONFIRMED!";
+                $message = "<p>Dear : ".ucfirst($fname)."</p><br/>";
+		
+		
+		$message .= "<table  border='1'><style>table {border-collapse: collapse;}table, td, th {padding:5px;border: 1px solid black;}</style>";
+		$message .= "<tr><td>Username </td>";
+		
+		$pieces = explode(",", $username);
+		foreach($pieces as $rowsuser){
+			$message .= "<td>".$rowsuser." </td>";
+		}
+		$message .= "</tr>";
+		
+		$message .= "<tr><td>Password </td>";
+
+		$pieces1 = explode(",", $password);
+		foreach($pieces1 as $rowspass){
+			$message .= "<td>".$rowspass." </td>";
+		}
+		
+		$message .= "</tr>";
+
+		$message .= "</table>";
+		
+		
+		
+		
+		$message .= "<p>If you have any questions please submit a support ticket.</p><br/><br/>";
+
+		$message .= "<p>Thanks,</p>";
+		$message .= "<p>The SapFund Team,<br/>Where wealth is predictable<br/><br/><br/>";
+		
+		
+		$message .= "<p><b>Do not reply</b> to this email. If you have any questions, please submit a support ticket.<br/>";
+		
+		$from_email = "info@sapfund.com";
+		
+		$headers  = "MIME-Version: 1.0\r\n";
+		$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+		$headers .= 'From: Sapfund <'.$from_email.'>' . "\r\n";
+		
+		mail($to,$subject,$message ,$headers);
 echo 'success';
 }else{
 echo 'failiure';
